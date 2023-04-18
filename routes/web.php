b452ea6ae62fee25346a7b69f454305d5e09e957
panxibei <panxibei@yandex.com>
2023-04-18 14:46:53 +0800

    pdfs

--- routes/web.php
+++ routes/web.php
@@ -23,26 +23,26 @@ use App\Http\Controllers\Test\testController;
 // 测试用
 // Route::group(['prefix'=>'test', 'namespace'=>'Test', 'middleware'=>['jwtauth','permission:permission_super_admin']], function() {
 Route::group(['prefix'=>'test', 'namespace'=>'Test', 'middleware'=>[]], function() {
-    Route::get('test', 'testController@test');
+    Route::get('test', [testController::class, 'test']);
     // Route::get('phpinfo', 'testController@phpinfo');
     Route::get('phpinfo', [testController::class, 'phpinfo']);
-    Route::get('ldap', 'testController@ldap');
-    Route::get('scroll', 'testController@scroll');
-    Route::get('mint', 'testController@mint');
-    Route::get('muse', 'testController@muse');
-    Route::get('vant', 'testController@vant');
-    Route::get('cube', 'testController@cube');
-    Route::get('pgsql', 'testController@pgsql');
-    Route::get('pdf', 'testController@pdf');
+    Route::get('ldap', [testController::class, 'ldap']);
+    Route::get('scroll', [testController::class, 'scroll']);
+    Route::get('mint', [testController::class, 'mint']);
+    Route::get('muse', [testController::class, 'muse']);
+    Route::get('vant', [testController::class, 'vant']);
+    Route::get('cube', [testController::class, 'cube']);
+    Route::get('pgsql', [testController::class, 'pgsql']);
+    Route::get('pdf', [testController::class, 'pdf']);
 
     // 测试camera
-    Route::get('camera', 'testController@camera');
-    Route::post('testCamera', 'testController@testCamera')->name('test.camera.testcamera');
+    Route::get('camera', [testController::class, 'camera']);
+    Route::post('testCamera', [testController::class, 'testCamera'])->name('test.camera.testcamera');
 
     // 测试邮件
-    Route::get('mail', 'testController@mail');
+    Route::get('mail', [testController::class, 'mail']);
 
     // 测试echarts
-    Route::get('echarts', 'testController@echarts');
+    Route::get('echarts', [testController::class, 'echarts']);
 });
     
